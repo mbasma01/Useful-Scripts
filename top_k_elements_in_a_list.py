9a6d5da783e5536cbc305d83c44ee2daa253f3c3
mbasma01 <mahmoudhbasma@gmail.com>
2024-07-16 23:16:38 +0300

    practice Talos day 2

--- top_k_elements_in_a_list.py
+++ top_k_elements_in_a_list.py
@@ -1,12 +1,20 @@
 def topKfrequent(nums,k):
     elements = {}
+    freq = [[] for _ in range(len(nums) + 1)]
     for i, n in enumerate(nums):
         elements[n] = 1 + elements.get(n, 0)
-    if len(elements.keys()) == k:
-        print(elements.keys())
-    frequent = [0]*k
+
+    for key, value in elements.items():
+        freq[value].append(key)
+
+    res = []
+
+    for i in range(len(freq) - 1, 0, -1):
+        for n in freq[i]:
+            res.append(n)
+            if len(res) == k:
+                return res
 
 
 nums = [1,2,2,3,3,4]
 topKfrequent(nums, 4)
-
