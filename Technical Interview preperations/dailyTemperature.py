ea0417d41d272634497f9a43f5af98ef365e120f
mbasma01 <mahmoudhbasma@gmail.com>
2024-07-21 00:24:37 +0300

    Update dailyTemperature.py

--- Technical Interview preperations/dailyTemperature.py
+++ Technical Interview preperations/dailyTemperature.py
@@ -1,4 +1,7 @@
 temps = [30,38,30,36,35,40,28]
+"""
+brute force O(n^2)
+
 def dailyTemp(temps):
     res = [0]*len(temps)
 
@@ -15,5 +18,19 @@ def dailyTemp(temps):
             else:
                 res[i] += 1
     return res
+"""
+
+"""Optimized to O(n) with O(n) space"""
+def dailyTemperatures(self, temperatures: List[int]) -> List[int]:
+    res = [0] * len(temperatures)
+    stack = []  # pair: [temp, index]
+
+    for i, t in enumerate(temperatures):
+        while stack and t > stack[-1][0]:
+            stackT, stackInd = stack.pop()
+            res[stackInd] = i - stackInd
+        stack.append((t, i))
+    return res
+
 print(dailyTemp(temps))
 
