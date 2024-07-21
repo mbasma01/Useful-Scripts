c6cf6054228486f8c74ca08b96b94778e7a98879
mbasma01 <mahmoudhbasma@gmail.com>
2024-07-21 16:38:04 +0300

    Update threeSum.py

--- Technical Interview preperations/threeSum.py
+++ Technical Interview preperations/threeSum.py
@@ -13,10 +13,11 @@ def threeSum(numbers):
             if s > 0:
                 r -= 1
             elif s < 0:
-                l -= 1
+                l += 1
             else:
                 res.append([n, numbers[l], numbers[r]])
                 l += 1
+                r -= 1
                 while numbers[l] == numbers[l -1] and l < r:
                     l += 1
     return res
