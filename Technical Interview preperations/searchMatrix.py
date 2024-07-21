183e9157ea38a8d4cd72cbf44fcf60ba4eca9978
mbasma01 <mahmoudhbasma@gmail.com>
2024-07-21 15:34:58 +0300

    Update searchMatrix.py

--- Technical Interview preperations/searchMatrix.py
+++ Technical Interview preperations/searchMatrix.py
@@ -1,8 +1,8 @@
 matrix = [[1,2,4,8],[10,11,12,13],[14,20,30,40]]
-target = 7
+target = 4
 
 """
-brute force
+brute force -- wrong, I could have traversed the matrix and save O(m*n) space
 
 def matrixToArray(matrix):
     m = len(matrix)
@@ -29,4 +29,42 @@ def search(matrix, target):
     return False
 
 """
+
+"""
+optimized approach
+"""
+
+def search(matrix, target):
+    m = len(matrix)
+    n = len(matrix[0])
+    l, r = 0, n - 1
+
+    # binary search to find the row that possibly has the target
+    while l <= r :
+        row = (l + r) //2
+        if matrix[row][0] < target:
+            l = row + 1
+        elif matrix[row][-1] > target:
+            r = row - 1
+        else:
+            break
+
+    row = (l + r)//2
+
+    l, r = 0, n - 1
+
+    if l > r:
+        return False # we didn't find a row statisfying the conditions
+
+        #binary search on the row we found
+    while l <= r:
+        m = (l + r)//2
+        if matrix[row][m] < target:
+            l = m + 1
+        elif matrix[row][m] > target:
+            r = m - 1
+        else:
+            return True
+    return False
+
 print(search(matrix, target))
